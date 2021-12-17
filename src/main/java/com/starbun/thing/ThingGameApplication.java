2bb7aa0c49aa886b6e43c1c362a5f680110097c4
Ramil Aminev <Ramil_Aminev@epam.com>
2021-12-17 20:08:22 +0400

    Initial commit

---
+++ src/main/java/com/starbun/thing/ThingGameApplication.java
@@ -0,0 +1,13 @@
+package com.starbun.thing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starbun/thing/ThingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbun.thing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThingGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
